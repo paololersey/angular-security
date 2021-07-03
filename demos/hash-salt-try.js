ac82d6e580054656ccdc728c2c58241f4b853a4a
Your Name <you@example.com>
2021-07-03 08:26:28 +0200

    change to argon

--- demos/hash-salt-try.js
+++ demos/hash-salt-try.js
@@ -4,8 +4,12 @@ var crypto = require('crypto');
 
 var password = 'monkey';
 
+var user1Salt = 1; // add a unique element for each user
+
+var user2Salt = 2;
+
 // we will use another hash other SHA-256 during the course, this is just for demo purposes
-var hash = crypto.createHash('sha256').update(password).digest('hex');
+var hash = crypto.createHash('sha256').update(password + user1Salt).digest('hex');
 
 console.log("The result of hashing " + password + " is:\n\n" + hash + "\n\n");
 
