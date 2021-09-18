8f94cea2ff2721a23870415a53b1e7a4b8f85ad5
paolo.spadoni <Paolo>
2021-09-18 07:23:47 +0200

    login as user

--- server/login-as-user.route.ts
+++ server/login-as-user.route.ts
@@ -1,9 +1,31 @@
-export function loginAsUser(req, res){
+import { db } from "./database";
+import { createSessionToken } from "./security.utils";
 
-    res.status(200).json({
+export function loginAsUser(req, res) {
+
+    // fake response
+    /*res.status(200).json({
         id:1,
         email: "temp@gmail.com",
         roles:["STUDENT"]
-    })
+    })*/
+
+    const impersonatedUserEmail = req.body.email;
+
+    const impersonatedUser = db.findUserByEmail(impersonatedUserEmail);
+
+    createSessionToken(impersonatedUser).then(token => {
+        res.cookie("SESSIONID", token, { httpOnly: true, secure: true });
+        res.status(200).json({
+            id: impersonatedUser.id,
+            email: impersonatedUser.email,
+            roles: impersonatedUser.roles
+        });
+    }
+
+    ).catch(err => {
+        console.log("Error tryying to login as a user", err);
+        res.sendStatus(500);
+    });
 
 }
